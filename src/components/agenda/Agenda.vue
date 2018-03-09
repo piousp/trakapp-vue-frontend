ec4a76725525c5e1be8ec09cf6780df700ddd797
ciris <fabio.flores@ciriscr.com>
2018-03-09 10:51:51 -0600

    feat: crear modulo para la agenda

---
+++ src/components/agenda/Agenda.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <full-calendar :events="events" />
+  </section>
+</template>
+
+<script>
+import $ from "jquery";
+window.jQuery = window.$ = $;
+import { FullCalendar } from 'vue-full-calendar'
+
+function data() {
+  return {
+    events: [
+      {
+        title  : 'event1',
+        start  : '2010-01-01',
+      },
+      {
+        title  : 'event2',
+        start  : '2010-01-05',
+        end    : '2010-01-07',
+      },
+      {
+        title  : 'event3',
+        start  : '2010-01-09T12:30:00',
+        allDay : false,
+      },
+    ],
+  }
+}
+
+export default {
+  components: {
+    FullCalendar,
+  },
+  data,
+}
+</script>
+
+<style lang="css">
+</style>
--- src/config/router.js
+++ src/config/router.js
@@ -6,6 +6,7 @@ import Home from "../components/Home.vue";
 import Login from "../components/Login.vue";
 import RutasMapa from "../components/mapa/rutas.js";
 import RutasEmpleados from "../components/empleados/rutas.js";
+// import RutasAgenda from "../components/agenda/rutas.js";
 import RutasDocumentacion from "../documentacion/rutas.js";
 
 const debug = D("ciris:config/router.js");
