5f6e9e8659c926d7b838714e533d2b238cb15205
Pablo Peraza <piousp@ciriscr.com>
2018-06-04 15:29:27 -0600

    VOLVER A CAMBIAR EL PUTO ARCHIVO EN LUGAR DE UNA VARIABLE DE ENTORNO COMO DEBE SER

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   API_URL: '"https://backend.trakapp.co.cr"',
-  SOCKET_URL: '"http://backend.trakapp.co.cr:9002"'
+  SOCKET_URL: '"https://backend.trakapp.co.cr"'
 }
