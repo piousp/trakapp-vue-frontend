a470d2216af9ac95edd941f78b89c978e2cb369c
Pablo Peraza <piousp@ciriscr.com>
2018-06-11 10:11:02 -0600

    Configurar el release stage de bugsnag

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   API_URL: '"https://backend.trakapp.co.cr"',
-  SOCKET_URL: '"https://backend.trakapp.co.cr"'
+  SOCKET_URL: '"https://backend.trakapp.co.cr"',
+  BUGSNAG_STAGE: '"production"'
 }
--- src/components/comunes/bugsnag.js
+++ src/components/comunes/bugsnag.js
@@ -5,5 +5,10 @@ const { version } = require("../../../package.json");
 const inst = bugsnag({
   apiKey: "c237c08021e14e4df9bc4b476f9f53ac",
   appVersion: version,
+  releaseStage: process.env.BUGSNAG_STAGE,
 });
+
+inst.app.version = version;
+inst.app.releaseStage = process.env.BUGSNAG_STAGE;
+
 export default inst;
