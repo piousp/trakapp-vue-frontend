df4deb541915976b1d5a6540bdb54315fc065f39
Pablo Peraza <piousp@ciriscr.com>
2018-09-17 11:00:48 -0600

    init

--- README.md
+++ README.md
@@ -1,61 +1,26 @@
-# Trakapp
-- Contruído con Node 8.9.4
-- Contruído con NPM 5.6.0
+# hello-world
 
-``` bash
+## Project setup
+```
 npm install
 ```
 
-## Cómo clonar el proyecto
-Hay 2 opciones: la regular y luego pasos extra, o con bandera.
-
-### Regular
-Se clona como siempre:
-``` bash
-git clone git@gitlab.com:ciris-base/frontend-vuejs.git
+### Compiles and hot-reloads for development
 ```
-Y luego de clonado, se deben ejecutar estos pasos dentro del repo:
-``` bash
-git submodule init
-git submodule update
+npm run serve
 ```
 
-### Con bandera
-Se clona como siempre pero se agrega una bandera al comando:
-``` bash
-git clone git@gitlab.com:ciris-base/frontend-vuejs.git --recurse-submodules
+### Compiles and minifies for production
 ```
-
-## Scripts de ejecución
-### Bump
-Realiza un aumento de version (major, minor, patch) y hace un commit
-``` bash
-npm version major|minor|patch
-```
-
-### Lint
-Verifica todos los archivos javascript dentro de `src` con las reglas definas en el linter.
-``` bash
-npm run lint
-```
-
-### Tests (Unidad)
-Ejecuta todos los tests que se encuentren dentro de `src`. Los archivos debe tener un nombre que cumpla con el siguiente regex: `\w+\.spec\.js`, o humanamente: `xxxx.spec.js`
-``` bash
-npm test
-```
-
-### Correr para desarrollo
-Ejecuta el linter y reinicia el proceso al encontrar cambios en el código
-``` bash
-npm start
-```
-
-### Transpilar para producción
-Compila y limpia todas las fuentes para usar en producción. El compilado queda en el directorio `dist`.
-Tamibién reduce el código con envify y uglify.
-``` bash
 npm run build
 ```
 
-For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
--- src/main.js
+++ src/main.js
@@ -1,37 +1,37 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import "babel-polyfill";
-import Vue from "vue";
-import "moment/locale/es";
-import Datetime from "vue-datetime";
-import VeeValidate from "vee-validate";
-import es from "vee-validate/dist/locale/es";
-import VueI18n from "vue-i18n";
-import * as VueGoogleMaps from "vue2-google-maps";
-import VueWebsocket from "vue-socket.io";
-import VueToastr from "@deveodk/vue-toastr";
-import bugsnagVue from "bugsnag-vue";
-import Multiselect from "vue-multiselect";
-import money from "v-money";
-import VueTheMask from "vue-the-mask";
-import App from "./App.vue";
-import router from "./config/router";
-import Auth from "./config/auth.js";
-import Notify from "./config/notify.js";
-import axios from "./config/axios.js";
-import messages from "./i18n";
-import vuexStore from "./config/store";
-import bugsnagClient from "./components/comunes/bugsnag";
-import AppModal from "./components/acciones/BaseModal.vue";
-import "./config/filtros.js";
-import "./components/comunes";
-import "./config/anno.js";
+import 'babel-polyfill';
+import Vue from 'vue';
+import 'moment/locale/es';
+import Datetime from 'vue-datetime';
+import VeeValidate from 'vee-validate';
+import es from 'vee-validate/dist/locale/es';
+import VueI18n from 'vue-i18n';
+import * as VueGoogleMaps from 'vue2-google-maps';
+import VueWebsocket from 'vue-socket.io';
+import VueToastr from '@deveodk/vue-toastr';
+import bugsnagVue from 'bugsnag-vue';
+import Multiselect from 'vue-multiselect';
+import money from 'v-money';
+import VueTheMask from 'vue-the-mask';
+import App from './App.vue';
+import router from './config/router';
+import Auth from './config/auth.js';
+import Notify from './config/notify.js';
+import axios from './config/axios.js';
+import messages from './i18n';
+import vuexStore from './config/store';
+import bugsnagClient from './components/comunes/bugsnag';
+import AppModal from './components/acciones/BaseModal.vue';
+import './config/filtros.js';
+import './components/comunes';
+import './config/anno.js';
 
 window.eventBus = new Vue({});
 
-const pkg = require("../package.json").name;
+const pkg = require('../package.json').name;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   bugsnagClient.use(bugsnagVue(Vue));
 }
 Vue.config.productionTip = false;
@@ -44,22 +44,22 @@ Vue.use(VueWebsocket, process.env.SOCKET_URL);
 Vue.use(money, { precision: 4 });
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyDn5jFd9F1zSo3XhhCD5r5bf3AQnpph5kI",
-    libraries: "geometry,visualization,places",
+    key: 'AIzaSyDn5jFd9F1zSo3XhhCD5r5bf3AQnpph5kI',
+    libraries: 'geometry,visualization,places',
   },
   autobindAllEvents: true,
 });
 Vue.use(Datetime);
 Vue.use(VueTheMask);
 
-Vue.component("app-modal", AppModal);
-Vue.component("infoWindow", VueGoogleMaps.InfoWindow);
-Vue.component("multiselect", Multiselect);
+Vue.component('app-modal', AppModal);
+Vue.component('infoWindow', VueGoogleMaps.InfoWindow);
+Vue.component('multiselect', Multiselect);
 
 Vue.prototype.$auth.checkAuth();
 
 const i18n = new VueI18n({
-  locale: "es",
+  locale: 'es',
   messages,
 });
 
@@ -73,10 +73,10 @@ Vue.use(VeeValidate, {
 
 /* eslint-disable no-new */
 export default new Vue({
-  el: "#app",
+  el: '#app',
   router,
   store: vuexStore,
   components: { App },
-  template: "<App/>",
+  render: h => h(App),
   i18n,
 });
