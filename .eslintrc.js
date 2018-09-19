a44978bfec71a61e9aa3957eb5316b7462a996fb
Pablo Peraza <piousp@ciriscr.com>
2018-09-19 14:02:25 -0600

    Importe inicial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,38 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    browser: true,
+    es6: true,
+    commonjs: true,
+    node: true
   },
+  plugins: [
+    "lodash",
+    "promise"
+  ],
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
+    "plugin:vue/recommended",
+    "@vue/airbnb",
+    "plugin:lodash/recommended",
+    "plugin:promise/recommended",
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "no-param-reassign": ["error", { "props": false }],
+    "import/extensions": ["always", {"ignorePackages": true}],
+    "no-use-before-define": ["error", { "functions": false, "classes": true }],
+    "no-underscore-dangle": [2,{"allow": ["_id"]}],
+    "prefer-promise-reject-errors": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/html-self-closing": ["error", {"html": {"void": "never"}}],
+    "lodash/prefer-lodash-method": "off",
+    "promise/no-callback-in-promise": "off",
+    "promise/no-nesting": "off"
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
   },
 };
--- README.md
+++ README.md
@@ -1,26 +1,40 @@
-# hello-world
+# Trakapp
+Frontend para clientes
 
-## Project setup
-```
+## Instalar todo
+``` bash
 npm install
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
+
+## Scripts de ejecución
+### Bump
+Realiza un aumento de version (major, minor, patch) y hace un commit
+``` bash
+npm version major|minor|patch
 ```
 
-### Compiles and minifies for production
+### Correr para desarrollo
+Ejecuta el linter y reinicia el proceso al encontrar cambios en el código
+``` bash
+npm start
 ```
+
+### Transpilar para producción
+Compila y limpia todas las fuentes para usar en producción. El compilado queda en el directorio `dist`.
+Tamibién reduce el código con envify y uglify.
+``` bash
 npm run build
 ```
 
-### Lints and fixes files
-```
+### Lint
+Verifica todos los archivos javascript dentro de `src` con las reglas definas en el linter.
+``` bash
 npm run lint
 ```
 
-### Run your unit tests
-```
+### Tests (Unidad)
+Ejecuta todos los tests que se encuentren dentro de `src`. Los archivos debe tener un nombre que cumpla con el siguiente regex: `\w+\.spec\.js`, o humanamente: `xxxx.spec.js`
+``` bash
 npm run test:unit
 ```
