7944508b5002780c5869bd415551a280c963800b
ciris <fabio.flores@ciriscr.com>
2018-09-19 10:28:16 -0600

    feat: subtarea, reporte de tareas y fixes - Poder agregar subtareas a la tarea - Poder filtrar por empleado los reportes de tareas - Unificar los archivos de reportes en uno solo - Arreglar error que botaba todo si uno solo tenía una cuenta asociada a su usuario - Arreglar cargado de tareas en la agenda - Arreglar botones de migrar cuenta que no salian

--- src/components/perfil/store.js
+++ src/components/perfil/store.js
@@ -52,7 +52,7 @@ function actualizarDatosCuenta(context, cuenta) {
 
 function cargarCuenta(context, params) {
   const { cuenta, recordarme } = params;
-  return perfilApi.getCuenta(cuenta._id)
+  return perfilApi.getCuenta(cuenta)
     .then((resp) => {
       context.commit("setCuenta", { cuenta: resp, recordarme });
       context.dispatch("empleados/cargarListado", null, { root: true });
